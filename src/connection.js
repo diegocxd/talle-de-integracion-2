348c8f066efac485b9fe5317d7e2045302d85d47
Sebastian <sesparza2020@alu.uct.cl>
2021-10-06 17:44:34 -0300

    creacion de archivo para probar mongo
    se crea archivo para probar la base de datos

---
+++ src/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://Seba:taller2@db-dcastro.lcmac.mongodb.net/test');
+
+
+mongoose.connection.once('open',function(){
+    console.log('Se ha establecido la conexion');
+}).on('error',function(error){
+    console.log('Error de conexion:', error);
+});
