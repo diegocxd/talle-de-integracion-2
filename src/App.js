1b675a0d281d5c577e9f83659ce593d67164010b
Sebastian <sesparza2020@alu.uct.cl>
2021-10-29 15:05:37 -0300

    Creacion de diseño Editor de texto y drag and drop
    Se crea el editor de texto usando el modulo WYSIWYG y beautiful dnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import TextEditor from './components/TextEditor';
+import { useState } from 'react';
+
+
+const initialTemplates = [
+  {
+    id:"1",
+    text:"plantilla 1"
+  },
+  {
+    id:"2",
+    text:"plantilla 2"
+  },
+  {
+    id:"3",
+    text:"plantilla 3"
+  },
+  {
+    id:"4",
+    text:"plantilla 4"
+  }
+];
 
 function App() {
+  const [templates, setTemplates] = useState(initialTemplates);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Text Editor</h1>
       </header>
+      <div className="container">
+        <div className="nav">
+          <nav>Nav</nav>
+        </div>
+        <div className="TextEditor">
+          <TextEditor/>
+        </div> 
+        <div className="plantillas">
+          <h1>Plantillas:</h1>
+          <ul className="templates-container">
+            {templates.map((template) => (
+              <li className="template-item" key={template.id}>{template.text}</li>
+              ))}
+          </ul>
+        </div>
+        </div>
     </div>
   );
 }
